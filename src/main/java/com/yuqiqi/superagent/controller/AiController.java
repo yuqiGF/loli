a1eac79b82ffb9b6fa4d4072f73c2041231f10ef
yuqiqi <2328441709@qq.com>
2026-01-06 06:06:05 +0800

    请求路径测试

--- src/main/java/com/yuqiqi/superagent/controller/AiController.java
+++ src/main/java/com/yuqiqi/superagent/controller/AiController.java
@@ -4,10 +4,7 @@ import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import com.yuqiqi.superagent.app.AnimeMaster;
 import com.yuqiqi.superagent.service.AiService;
 import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/ai")
@@ -15,8 +12,14 @@ public class AiController {
     @Resource
     private AiService aiService;
 
+    // 请求路径示例 http://localhost:8123/api/ai/AnimeMaster?message=
     @PostMapping("/AnimeMaster")
     public String anime(@RequestParam String message){
+        return aiService.chat(message);
+    }
+
+    @GetMapping("/AnimeMaster")
+    public String test(String message){
         return aiService.chat(message);
     }
 }
